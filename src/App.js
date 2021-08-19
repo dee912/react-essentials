8f5c54f881aa9f45bc37251b9b2ed89e9ac999ae
dee912 <devantespaine@gmail.com>
2021-08-19 13:23:19 +0100

    components & props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {
+  console.log(props)
+  
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Devante" />
+      <Main adjective="amazing" />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
