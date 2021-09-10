3410ee1566e5e57add9f5ef7a0b89aaded84b170
dee912 <devantespaine@gmail.com>
2021-09-10 20:47:47 +0100

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
   const [emotion, setEmotion] = useState('happy')
+  const [secondary, setSecondaty] = useState('tired')
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`)
+  }, [emotion])
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary])
 
   return (
     <>
-    <h1>Current emotion is {emotion}.</h1>
-    <button onClick={() => setEmotion("happy")}>Happy</button>
+    <h1>Current emotion is {emotion} and {secondary}.</h1>
+    <button onClick={() => setEmotion("happy")}>Make Happy</button>
+    <button onClick={() => setSecondaty("crabby")}>Crabby</button>
     <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-    <button onClick={() => setEmotion("enthusiastic")}>Enthusiastic</button>
+    <button onClick={() => setEmotion("enthusiastic")}>Enthus</button>
+    
     </>
   )
   
