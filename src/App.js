dfd51d516957940681ca00ddd3fb90ce976850f1
dee912 <devantespaine@gmail.com>
2021-09-10 20:28:31 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
 import './App.css'
-import restaurant from './restaurant.jpeg'
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  )
+function SecretComponent() {
+  return <h1>Super secret information for auth only</h1>
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <img src={restaurant} alt="restaurant layout"/>
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => 
-        <li key={dish.id}>
-          {dish.title}
-        </li>
-        )}
-      </ul>
-    </section>
-  )
+function RegularComponent() {
+  return <h1>Everyone can see</h1>
 }
-
-function Footer(props) {
+function App(props) {
   return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
+    <>
+    {props.authorized ? <SecretComponent /> : <RegularComponent />}
+    </>
   )
-}
-
-const dishes = [
-  'Mac and Cheese',
-  'Salmon',
-  'Cookies',
-  'Chips'
-]
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }))
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Devante" />
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+  
 }
 
 export default App;
